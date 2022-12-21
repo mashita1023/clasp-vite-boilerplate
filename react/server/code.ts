9884f23a1af6d5116a95a7cf5151af07d578e5bd
mashita1023 <toshichika-mashimo@retty.me>
2022-12-21 14:46:48 +0900

    add react

---
+++ react/server/code.ts
@@ -0,0 +1,5 @@
+export function doGet() {
+  return HtmlService.createHtmlOutputFromFile("hosting/index.html")
+    .addMetaTag("viewport", "width=device-width, initial-scale=1")
+    .setTitle("React + GAS");
+}
